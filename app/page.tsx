e30f5f9b1040110fa18e7dfa0a464f5e7918bb32
Aryan Yadav <aryan31424@gmail.com>
2025-03-26 23:32:23 +0530

    Portfolio completed but some changes are still required

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,28 @@
+import Achievements from "@/components/Achievements";
+import Approach from "@/components/Approach";
+import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Primary from "@/components/Primary";
+import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
+import { navItems } from "@/data/index";
 import Image from "next/image";
 import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center
-    items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 ">
+    items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={[{name: "Home", link: '/', icon: <FaHome/>}]}/>
+        <FloatingNav navItems={navItems}/>
         <Primary/>
         <Grid/>
+        <RecentProjects/>
+        <Achievements/>
+        <Experience/>
+        <Approach/>
+        <Footer/>
       </div>
     </main>
   );
