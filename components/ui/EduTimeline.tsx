8d7056ae0a33e28cdd4371004d53b030124e2c8a
Aryan Yadav <aryan31424@gmail.com>
2025-03-30 15:24:03 +0530

    EduTimeline also updated

--- components/ui/EduTimeline.tsx
+++ components/ui/EduTimeline.tsx
@@ -1,16 +1,7 @@
+// Component from Flowbite UI
+
 import * as React from "react";
-import Timeline from "@mui/lab/Timeline";
-import TimelineItem from "@mui/lab/TimelineItem";
-import TimelineSeparator from "@mui/lab/TimelineSeparator";
-import TimelineConnector from "@mui/lab/TimelineConnector";
-import TimelineContent from "@mui/lab/TimelineContent";
-import TimelineOppositeContent from "@mui/lab/TimelineOppositeContent";
-import TimelineDot from "@mui/lab/TimelineDot";
-import AccountBalanceIcon from "@mui/icons-material/AccountBalance";
-import LaptopMacIcon from "@mui/icons-material/LaptopMac";
-import SchoolIcon from "@mui/icons-material/School";
-import RepeatIcon from "@mui/icons-material/Repeat";
-import Typography from "@mui/material/Typography";
+
 
 export default function CustomizedTimeline() {
   return (
